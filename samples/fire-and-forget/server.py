ce82e13a2b8d1ad1c5c2506d1c783d157c718d46
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-12-01 09:58:49 +0900

    add server sample

--- samples/fire-and-forget/server.py
+++ samples/fire-and-forget/server.py
@@ -5,28 +5,38 @@
     Resources:
         - https://kuttsun.blogspot.com/2020/03/python-fire-and-forget.html
         - https://pod.hatenablog.com/entry/2019/03/21/162511
+
+    Run server:
+        cd $APP_DIR
+        uvicorn server:app --reload
+
+    Request:
+        curl -X GET "localhost:8000/"
 """
 import asyncio
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 import time
 
+from fastapi import FastAPI
 
-def heavy_proc(proc_time: int, st: int=0):
-    print(f"-- heavy start {time.time() - st}s")
-    time.sleep(proc_time)
-    print(f"-- heavy end {time.time() - st}s")
+from main import heavy_proc
 
-def run():
+app = FastAPI()
+
+@app.get('/')
+def read_root():
     st = time.time()
-    loop = asyncio.get_event_loop()
-    executor = ProcessPoolExecutor(max_workers=3)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
     print(f"MAIN: start {time.time() - st}s")
+    executor = ProcessPoolExecutor(max_workers=3)
     loop.run_in_executor(executor, partial(heavy_proc, 3, st))
-    print(f"MAIN: end {time.time() - st}s")
-    time.sleep(10)
     print(f"MAIN: exit {time.time() - st}s")
+    return {"greeting": f"Hello fastapi {time.time() - st}s"}
 
-if __name__ == "__main__":
-    run()
 
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: str = None):
+    return {"item id": item_id, "query": q}

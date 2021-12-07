a67e1365ff27bff50505844aea33a6a5ed7f9c35
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-12-07 19:44:43 +0900

    update proto building script, add route guide

--- samples/grpc-getting-started/route_guide-py/build_prots.sh
+++ samples/grpc-getting-started/route_guide-py/build_prots.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 # exec in virtual env
-PROTO_NAME="helloworld"
+PROTO_NAME="route_guide"
 python -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/$PROTO_NAME.proto

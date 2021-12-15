295d2bebd511e271ebf98b72ef2cc5ab06976ad3
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-12-15 13:30:27 +0900

    add tutorial code

---
+++ samples/logger-py/cookbook_tutorial.py
@@ -0,0 +1,34 @@
+"""Tutorial
+
+    - https://docs.python.org/ja/3.9/howto/logging-cookbook.html#logging-cookbook
+"""
+
+import logging
+import cookbook_tutorial_sub
+
+# create logger with 'spam_application'
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info('creating an instance of auxiliary_module.Auxiliary')
+a = cookbook_tutorial_sub.Auxiliary()
+logger.info('created an instance of auxiliary_module.Auxiliary')
+logger.info('calling auxiliary_module.Auxiliary.do_something')
+a.do_something()
+logger.info('finished auxiliary_module.Auxiliary.do_something')
+logger.info('calling auxiliary_module.some_function()')
+cookbook_tutorial_sub.some_function()
+logger.info('done with auxiliary_module.some_function()')

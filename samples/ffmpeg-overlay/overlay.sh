54744feefbb184770b948e1e7cf63fe758ae1d34
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-12-27 17:13:56 +0900

    add chunklist style input

--- samples/ffmpeg-overlay/overlay.sh
+++ samples/ffmpeg-overlay/overlay.sh
@@ -15,3 +15,9 @@ ffmpeg -i ./data/input.mp4 -i ./data/yt_logo_rgb_light.png \
 ./data/output.mp4 -y
 
 # ffmpeg -i ./data/input.mp4 -i ./data/yt_logo_rgb_light.png -filter_complex "[0]trim=start_frame=10:end_frame=20[v0];[0]trim=start_frame=30:end_frame=40[v1];[v0][v1]concat=n=2[v2];[1]hflip[v3];[v2][v3]overlay=eof_action=repeat[v4];[v4]drawbox=50:50:120:120:red:t=5[v5]" -map [v5] output.mp4
+
+# from chunklist - failed
+# ffmpeg -f concat -i ./data/chunklist.txt -i ./data/yt_logo_rgb_light.png \
+# -filter_complex "[0:v][1:v] overlay=100:25:enable='between(t,1,10)'" \
+# -pix_fmt yuv420p -c:a copy \
+# ./data/output-con.mp4 -y

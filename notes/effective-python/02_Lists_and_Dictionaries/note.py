599602aaac7255446569d7e89ecf05ec03b5721b
Sota Masuda <sota.masuda@the-lionsdata.com>
2022-01-22 18:12:38 +0900

    Update Section02-Item14

--- notes/effective-python/02_Lists_and_Dictionaries/note.py
+++ notes/effective-python/02_Lists_and_Dictionaries/note.py
@@ -49,19 +49,52 @@ def complex_sort_with_key_arg() -> None:
 
     tools = [
         Tool('level', 3.5),
+        Tool('level', 4.6),
         Tool('hammer', 1.25),
+        Tool('hammer', 4.6),
+        Tool('hammer', 3.5),
+        Tool('screwdriver', 1.25),
         Tool('screwdriver', 0.5),
-        Tool('chisel', 0.25)
+        Tool('chisel', 1.25),
+        Tool('chisel', 0.25),
+        Tool('chisel', 3.5)
     ]
     print("Unsorted(name, weight): \n", tools)
+
     print("Effective sorting with name")
     sorted_with_name = tools
     sorted_with_name.sort(key=lambda x: x.name)
-    print("tools.sort(key=lambda x: x.name) :\n", sorted_with_name)
+    print(" | tools.sort(key=lambda x: x.name) :\n", sorted_with_name)
+
     print("Effective sorting with weight")
     sorted_with_weight = tools
     sorted_with_weight.sort(key=lambda x: x.weight)
-    print("tools.sort(key=lambda x: x.name) :\n", sorted_with_weight)
+    print(" | tools.sort(key=lambda x: x.name) :\n", sorted_with_weight)
+
+    print("Effective complex sorting with name/weight")
+    print(" | (1) name-ASC > weight-DESC")
+    sorted_with_nw_1 = tools
+    sorted_with_nw_1.sort(key=lambda x: (x.name, -x.weight))
+    print(" | - tools.sort(key=lambda x: (x.name, -x.weight)):\n", sorted_with_nw_1)
+
+    print(" | (2) name-DESC > weight-ASC")
+    sorted_with_nw_2 = tools
+    # reverse affects both of name/weight attributes.
+    # str can't use `negative` for DESCENDING
+    sorted_with_nw_2.sort(key=lambda x: (x.name, -x.weight), reverse=True)
+    print(" | - tools.sort(key=lambda x: (x.name, -x.weight), reverse=True):\n", sorted_with_nw_2)
+
+    print(" | (3) weight-ASC > name-DESC")
+    sorted_with_nw_3 = tools
+    # key priority is decided by tuple order
+    sorted_with_nw_3.sort(key=lambda x: (-x.weight, x.name), reverse=True)
+    print(" | - tools.sort(key=lambda x: (-x.weight, x.name), reverse=True):\n", sorted_with_nw_3)
+
+    print(" | (4) weight-DESC > name-DESC")
+    sorted_with_nw_4 = tools
+    # reverse affects both of name/weight attributes.
+    sorted_with_nw_4.sort(key=lambda x: (x.weight, x.name), reverse=True)
+    print(" | - tools.sort(key=lambda x: (-x.weight, x.name), reverse=True):\n", sorted_with_nw_4)
 
 
 if __name__ == "__main__":

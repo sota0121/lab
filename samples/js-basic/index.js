2f111075df850e3c3d7fd771d6b4397410797d3d
sota_masuda <sota.masuda@the-lionsdata.com>
2022-05-17 20:38:02 +0900

    add 分割代入 / spread

--- samples/js-basic/index.js
+++ samples/js-basic/index.js
@@ -79,3 +79,62 @@ const f4_arrow = (val1, val2) => (
 )
 console.log(`f4_arrow("foo", 10) = ${JSON.stringify(f4_arrow("foo", 10))}`);
 
+
+// ==========================================================================
+// 分割代入
+// ==========================================================================
+console.log('===== 分割代入 ======');
+console.log('> オブジェクト')
+const myProfile = {
+    name: 'foo',
+    age: 10
+}
+console.log(`myProfile = ${JSON.stringify(myProfile)}`);
+const { name, age } = myProfile;
+console.log('const { name, age } = myProfile;');
+console.log(`name = ${name} / age = ${age}`);
+
+console.log('> 配列')
+const myArray = ['adam', 'male', 20];
+console.log(myArray)
+const [name0] = myArray
+const [name1, gender0] = myArray
+const [name2, gender1, age1] = myArray
+
+console.log(`name0 = ${name0}`)
+console.log(`name1 = ${name1} / gender0 = ${gender0}`)
+console.log(`name2 = ${name2} / gender1 = ${gender1} / age1 = ${age1}`)
+
+console.log('> 存在しない要素など、デフォルト値を設定できる')
+const [name3, gender2, age2, desc = 'description'] = myArray
+console.log(`name3 = ${name3} / gender2 = ${gender2} / age2 = ${age2} / desc = ${desc}`)
+
+
+// ==========================================================================
+// スプレッド構文
+// ==========================================================================
+console.log('===== スプレッド構文 ======');
+console.log('> 要素を展開する (like xargs)')
+const myArray1 = [1, 2, 3];
+console.log(`myArray1 = ${myArray1}`);
+console.log('...myArray1 = ');
+console.log(...myArray1);
+
+console.log('> 要素をひとまとめにする（like unpack all syntax in python）')
+const myArray2 = [1, 2, 3, 4, 5];
+console.log(`myArray2 = ${myArray2}`);
+const [n1, n2, ...rest] = myArray2;
+console.log('const [n1, n2, ...rest] = myArray2;');
+console.log(`n1 = ${n1} / n2 = ${n2} / rest = ${rest}`);
+
+console.log('')
+console.log('> 配列を安全にコピーすることができる（イコールコピーだと参照になってしまう）')
+const myArray3 = [...myArray2];
+console.log('const myArray3 = [...myArray2];');
+console.log(`myArray3 = ${myArray3}`);
+
+console.log('')
+console.log('> 配列を安全に結合することができる')
+const myArray4 = [...myArray2, ...myArray3];
+console.log('const myArray4 = [...myArray2, ...myArray3];');
+console.log(`myArray4 = ${myArray4}`);
